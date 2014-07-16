6e424229842b67b299051940021ea881f73f9ce7
tehnik819 <tehnik.819@gmail.com>
2014-07-16 16:17:24 +0700

    Java - Practical task - 1 - complete

--- src/com/noveogroup/device/Device.java
+++ src/com/noveogroup/device/Device.java
@@ -3,6 +3,6 @@ package com.noveogroup.device;
 /**
  * This interface represents a Device for sorting algorithms.
  */
-public interface Device extends Comparable<Device> {
+public interface Device {
     int getPrice();
 }
